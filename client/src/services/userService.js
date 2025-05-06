684478538c528ebe9797fbd4c7e3687706c87563
Utkarsh Garg <utkarshgarg0903@yahoo.in>
2025-05-06 12:11:25 +0530

    backend url

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export async function getUsers(page = 1, limit = 50) {
-  const response = await axios.get(`https://localhost:5000/api/users?page=${page}&limit=${limit}`);
+  const response = await axios.get(`https://markopolo-ai-assignment.onrender.com/api/users?page=${page}&limit=${limit}`);
   return response.data.data || [];
 }
